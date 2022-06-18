2fb0f166dae7dd02db3789277c144cfe8be88964
roedyrustam <roedyrustam.id@gmail.com>
2022-06-19 02:01:20 +0800

    master

---
+++ config.js
@@ -0,0 +1,26 @@
+// WebToDesk v2.0.0
+// Sameera Damith (CodeStack)
+// damith.sameera1@gmail.com
+
+const config = {
+    // Main Application URL
+    'websiteUrl' : 'http://example.com',
+
+    // Application Name
+    'appName' : 'WebToDesk',
+
+    // Application window width and height
+    'width' : 1280,
+    'height' : 800,
+    'minWidth' : 1280,
+    'minHeight' : 800,
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
